39574f9b56f031959199d196024a04dbb1a37d2d
Mateus Macedo Dos Anjos <mateus.capitani@qsaude.com.br>
2022-06-10 11:38:39 -0300

    fix(config): config module init vars

--- src/main.module.ts
+++ src/main.module.ts
@@ -5,11 +5,11 @@ import { Module } from '@nestjs/common'
   imports: [
     CommonModule.register({
       configModule: {
-        ignoreEnvFile: false,
-        envFilePath: '.env',
-        expandVariables: true,
-        cache: true,
-        isGlobal: true
+        ignoreEnvFile: JSON.parse(process.env.IGNORE_ENV_FILE),
+        envFilePath: JSON.parse(process.env.ENV_FILE_PATH),
+        expandVariables: JSON.parse(process.env.EXPAND_VARIABLES),
+        cache: JSON.parse(process.env.CACHE),
+        isGlobal: JSON.parse(process.env.IS_GLOBAL)
       }
     })
   ],
