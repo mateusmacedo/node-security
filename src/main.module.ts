31d5da2876fc7cefd2f240b9054233d8e86de79c
Mateus Macedo Dos Anjos <mateus.capitani@qsaude.com.br>
2022-06-10 11:52:17 -0300

    fix(config): correct env parse

--- src/main.module.ts
+++ src/main.module.ts
@@ -6,7 +6,7 @@ import { Module } from '@nestjs/common'
     CommonModule.register({
       configModule: {
         ignoreEnvFile: JSON.parse(process.env.IGNORE_ENV_FILE),
-        envFilePath: JSON.parse(process.env.ENV_FILE_PATH),
+        envFilePath: process.env.ENV_FILE_PATH,
         expandVariables: JSON.parse(process.env.EXPAND_VARIABLES),
         cache: JSON.parse(process.env.CACHE),
         isGlobal: JSON.parse(process.env.IS_GLOBAL)
