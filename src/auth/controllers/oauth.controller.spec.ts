8df76f646f516c426d433cb25674ce36fff38e1d
Mateus Macedo Dos Anjos <mateus.capitani@qsaude.com.br>
2022-06-10 21:42:57 -0300

    test(login): ensure strategyRegistry.validate calls correctly

--- src/auth/controllers/oauth.controller.spec.ts
+++ src/auth/controllers/oauth.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Oauth2Controller } from '@app/auth/controllers'
+import { OAuth2Request } from '@app/auth/dto'
 import { Oauth2GrantStrategyRegistryService } from '@app/auth/strategies'
 import { createMock } from '@golevelup/nestjs-testing'
 import { Test, TestingModule } from '@nestjs/testing'
@@ -6,10 +7,13 @@ import { Test, TestingModule } from '@nestjs/testing'
 describe('OauthController', () => {
   let sut: Oauth2Controller
   let strategyRegistry: Oauth2GrantStrategyRegistryService
+  let request: OAuth2Request
 
   beforeEach(async () => {
     jest.clearAllMocks()
-    const strategyRegistryMock = createMock<Oauth2GrantStrategyRegistryService>()
+    const strategyRegistryMock = createMock<Oauth2GrantStrategyRegistryService>({
+      validate: jest.fn().mockResolvedValue(undefined)
+    })
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [Oauth2GrantStrategyRegistryService],
@@ -26,5 +30,13 @@ describe('OauthController', () => {
   it('should be defined', () => {
     expect(sut).toBeDefined()
     expect(strategyRegistry).toBeDefined()
+  })
+  describe('token', () => {
+    it('should validate request grant type', async () => {
+      request = createMock<OAuth2Request>()
+      await sut.token(request)
+      expect(strategyRegistry.validate).toHaveBeenCalledTimes(1)
+      expect(strategyRegistry.validate).toHaveBeenCalledWith(request)
+    })
   })
 })
