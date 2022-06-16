7791d203ec9d9fd7316ac4d821f0964a802f2da9
Mateus Macedo Dos Anjos <mateus.capitani@qsaude.com.br>
2022-06-16 02:56:04 -0300

    test(auth): ensure identifyClient successfully

--- src/auth/services/clients/cognito-identity-provider.service.spec.ts
+++ src/auth/services/clients/cognito-identity-provider.service.spec.ts
@@ -1,42 +1,82 @@
 import { IDENTITY_PROVIDER_METADATA } from '@app/auth/constants'
 import { IdentityContext } from '@app/auth/enums'
+import { CognitoIdentityProviderClientWithPoolId, IdentityProviderClient } from '@app/auth/interfaces'
 import { IdentityProviderService } from '@app/auth/services'
 import { CognitoIdentityProviderService } from '@app/auth/services/clients'
 import { IdentityProvider } from '@app/auth/services/clients/decorator'
 import { StrategyExplorerService } from '@app/common/services'
-import { CognitoIdentityProviderClient } from '@aws-sdk/client-cognito-identity-provider'
+import {
+  CognitoIdentityProviderClient,
+  DescribeUserPoolClientCommand,
+  DescribeUserPoolClientCommandOutput
+} from '@aws-sdk/client-cognito-identity-provider'
+import { createMock } from '@golevelup/nestjs-testing'
 import { Injectable } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
 
+jest.mock('@aws-sdk/client-cognito-identity-provider', () => {
+  const original = jest.requireActual('@aws-sdk/client-cognito-identity-provider')
+  return {
+    ...original,
+    DescribeUserPoolClientCommand: jest.fn(),
+    DescribeUserPoolClientCommandOutput: jest.fn()
+  }
+})
+
 @Injectable()
 @IdentityProvider(IdentityContext.AP)
-class IdentityProviderClientServiceStub extends CognitoIdentityProviderClient {
-  constructor(private readonly configService: ConfigService) {
-    super({
-      region: configService.get('AWS_DEFAULT_REGION'),
-      credentials: {
-        accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY')
-      },
-      endpoint:
-        configService.get('AWS_COGNITO_IDENTITY_PROVIDER_ENDPOINT') + configService.get('AWS_COGNITO_AP_USER_POOL_ID')
-    })
+class IdentityProviderClientServiceStub implements CognitoIdentityProviderClientWithPoolId {
+  constructor(private readonly userPoolId: string, private readonly client: CognitoIdentityProviderClient) {}
+  getUserPoolId(): string {
+    return this.userPoolId
+  }
+  getClient(): CognitoIdentityProviderClient {
+    return this.client
   }
 }
 
 describe('CognitoIdentityProviderService', () => {
   let cognitoIdentityProviderService: CognitoIdentityProviderService
+  let identityProviderClientServiceStub: IdentityProviderClientServiceStub
   let explorer: StrategyExplorerService
   let module: TestingModule
+  let identifyCommandOutput: DescribeUserPoolClientCommandOutput
   beforeEach(async () => {
     jest.clearAllMocks()
+    identifyCommandOutput = createMock<DescribeUserPoolClientCommandOutput>({
+      $metadata: undefined,
+      UserPoolClient: {
+        AllowedOAuthFlows: [],
+        AllowedOAuthScopes: [],
+        ClientId: 'clientId',
+        ClientName: 'clientName',
+        ClientSecret: 'clientSecret',
+        ExplicitAuthFlows: [],
+        UserPoolId: 'userPoolId'
+      }
+    })
     module = await Test.createTestingModule({
-      imports: [ConfigModule],
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: 'test.env',
+          expandVariables: true
+        })
+      ],
       controllers: [],
       providers: [
         StrategyExplorerService,
-        IdentityProviderClientServiceStub,
+        {
+          inject: [ConfigService],
+          provide: IdentityProviderClientServiceStub,
+          useFactory: (configService: ConfigService) => {
+            const client = createMock<CognitoIdentityProviderClient>({
+              send: jest.fn().mockResolvedValue(identifyCommandOutput)
+            })
+            const userPoolId = configService.get('AWS_COGNITO_AP_USER_POOL_ID')
+            return new IdentityProviderClientServiceStub(userPoolId, client)
+          }
+        },
         {
           provide: IdentityProviderService,
           useClass: CognitoIdentityProviderService
@@ -48,11 +88,15 @@ describe('CognitoIdentityProviderService', () => {
       IdentityProviderService
     ) as CognitoIdentityProviderService
     explorer = module.get<StrategyExplorerService>(StrategyExplorerService)
-    cognitoIdentityProviderService.register(explorer.explore<CognitoIdentityProviderClient>(IDENTITY_PROVIDER_METADATA))
+    identityProviderClientServiceStub = module.get<IdentityProviderClientServiceStub>(IdentityProviderClientServiceStub)
+    cognitoIdentityProviderService.register(
+      explorer.explore<CognitoIdentityProviderClientWithPoolId>(IDENTITY_PROVIDER_METADATA)
+    )
   })
 
   it('should be defined', () => {
     expect(cognitoIdentityProviderService).toBeDefined()
+    expect(identityProviderClientServiceStub).toBeDefined()
     expect(explorer).toBeDefined()
   })
   describe('register', () => {
@@ -61,7 +105,7 @@ describe('CognitoIdentityProviderService', () => {
       const reflect = jest.spyOn(Reflect, 'getMetadata')
       cognitoIdentityProviderService['registry'] = {}
       cognitoIdentityProviderService.register(
-        explorer.explore<CognitoIdentityProviderClient>(IDENTITY_PROVIDER_METADATA)
+        explorer.explore<CognitoIdentityProviderClientWithPoolId>(IDENTITY_PROVIDER_METADATA)
       )
       expect(cognitoIdentityProviderService['registry'][IdentityContext.AP]).toBeDefined()
       expect(getSpy).toHaveBeenCalledTimes(1)
@@ -75,6 +119,21 @@ describe('CognitoIdentityProviderService', () => {
       expect(cognitoIdentityProviderService['registry']).toEqual({})
       expect(getSpy).toHaveBeenCalledTimes(1)
       expect(getSpy).toHaveBeenCalledWith(IdentityProviderClientServiceStub, { strict: false })
+    })
+  })
+  describe('identifyClient', () => {
+    it('should identifyClient successfully', async () => {
+      const clientData: Partial<IdentityProviderClient> = {
+        clientId: '3e67tihr5n17r4g1nkoi18s8bq',
+        identityContext: IdentityContext.AP
+      }
+      const result = await cognitoIdentityProviderService.identifyClient(clientData)
+      expect(result).toBeDefined()
+      expect(result.clientId).toEqual(identifyCommandOutput.UserPoolClient.ClientId)
+      expect(result.clientName).toEqual(identifyCommandOutput.UserPoolClient.ClientName)
+      expect(result.clientSecret).toEqual(identifyCommandOutput.UserPoolClient.ClientSecret)
+      expect(result.clientScopes).toEqual(identifyCommandOutput.UserPoolClient.AllowedOAuthScopes)
+      expect(result.allowedAuthFlow).toEqual(identifyCommandOutput.UserPoolClient.AllowedOAuthFlows)
     })
   })
 })
