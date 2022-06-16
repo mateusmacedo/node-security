23290c518fefc48e70763563a03ad126741887ce
Mateus Macedo Dos Anjos <mateus.capitani@qsaude.com.br>
2022-06-16 02:55:20 -0300

    feat(auth): ensure identifyClient successfully

--- src/auth/services/clients/cognito-identity-provider.service.ts
+++ src/auth/services/clients/cognito-identity-provider.service.ts
@@ -1,23 +1,27 @@
 import { IDENTITY_PROVIDER_METADATA } from '@app/auth/constants'
 import { OAuth2Request } from '@app/auth/dtos'
-import { IdentifyProviderClient, IdentityProviderClient, StrategyRegistry } from '@app/auth/interfaces'
+import { CognitoIdentityProviderClientWithPoolId, IdentityProviderClient, StrategyRegistry } from '@app/auth/interfaces'
 import { IdentityProviderService } from '@app/auth/services'
+import {
+  DescribeUserPoolClientCommand,
+  DescribeUserPoolClientCommandInput
+} from '@aws-sdk/client-cognito-identity-provider'
 import { Injectable, Type } from '@nestjs/common'
 import { ModuleRef } from '@nestjs/core'
 
-type IdentityProviderClientType = Type<IdentifyProviderClient>
+type CognitoIdentityProviderClientType = Type<CognitoIdentityProviderClientWithPoolId>
 @Injectable()
 export class CognitoIdentityProviderService
   extends IdentityProviderService
-  implements StrategyRegistry<IdentityProviderClientType>
+  implements StrategyRegistry<CognitoIdentityProviderClientType>
 {
-  private registry: { [s: string]: IdentifyProviderClient } = {}
+  private registry: { [s: string]: CognitoIdentityProviderClientWithPoolId } = {}
 
   constructor(private readonly moduleRef: ModuleRef) {
     super()
   }
 
-  register(strategies): void {
+  register(strategies: CognitoIdentityProviderClientType[]): void {
     strategies.forEach((strategy) => {
       const instance = this.moduleRef.get(strategy, {
         strict: false
@@ -26,7 +30,7 @@ export class CognitoIdentityProviderService
         return
       }
       const strategyName = Reflect.getMetadata(IDENTITY_PROVIDER_METADATA, strategy)
-      this.registry[strategyName] = instance as IdentifyProviderClient
+      this.registry[strategyName] = instance
     })
   }
 
@@ -34,7 +38,20 @@ export class CognitoIdentityProviderService
     throw new Error('Method not implemented.')
   }
 
-  identifyClient(data: Partial<IdentityProviderClient>): Promise<IdentityProviderClient> {
-    throw new Error('Method not implemented.')
+  async identifyClient(data: Partial<IdentityProviderClient>): Promise<IdentityProviderClient> {
+    const { identityContext, clientId } = data
+    const identifyClientCommandInput: DescribeUserPoolClientCommandInput = {
+      UserPoolId: this.registry[identityContext].getUserPoolId(),
+      ClientId: clientId
+    }
+    const identifyClientCommand = new DescribeUserPoolClientCommand(identifyClientCommandInput)
+    const result = await this.registry[identityContext].getClient().send(identifyClientCommand)
+    return {
+      allowedAuthFlow: result.UserPoolClient.AllowedOAuthFlows,
+      clientId: result.UserPoolClient.ClientId,
+      clientName: result.UserPoolClient.ClientName,
+      clientScopes: result.UserPoolClient.AllowedOAuthScopes,
+      clientSecret: result.UserPoolClient.ClientSecret
+    } as IdentityProviderClient
   }
 }
