2f81a38a639c542c8ed31f437e3e40d70ea7859c
Mateus Macedo Dos Anjos <mateus.capitani@qsaude.com.br>
2022-06-17 15:31:41 -0300

    chore(auth): register providers from module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,62 @@
+import { GRANT_STRATEGY_METADATA, IDENTITY_PROVIDER_METADATA } from '@app/auth/constants'
 import { Oauth2Controller } from '@app/auth/controllers/auth.controller'
-import { Oauth2GrantStrategyRegistry } from '@app/auth/services/strategies'
-import { Module } from '@nestjs/common'
+import {
+  GrantStrategyInterface,
+  IdentityProviderClientInterface,
+  IdentityProviderClientType
+} from '@app/auth/interfaces'
+import { GrantStrategyRegistry } from '@app/auth/services'
+import { ApIdentityProviderClientService, IdentityProviderService } from '@app/auth/services/providers'
+import { AbstractIdentityProviderService } from '@app/auth/services/providers/abstract'
+import { ClientCredentialGrantStrategyService } from '@app/auth/services/strategies'
+import { StrategyExplorerService } from '@app/common/services'
+import { CognitoIdentityProviderClient } from '@aws-sdk/client-cognito-identity-provider'
+import { Module, OnModuleInit } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 @Module({
-  controllers: [Oauth2Controller],
-  providers: [Oauth2GrantStrategyRegistry]
+  providers: [
+    StrategyExplorerService,
+    GrantStrategyRegistry,
+    {
+      inject: [ConfigService],
+      provide: 'AP_USER_POOL_ID',
+      useFactory: (configService: ConfigService) => configService.get('AWS_COGNITO_AP_USER_POOL_ID')
+    },
+    {
+      inject: [ConfigService],
+      provide: 'AP_PROVIDER_CLIENT',
+      useFactory: (configService: ConfigService) => {
+        const config = {
+          region: configService.get('AWS_DEFAULT_REGION'),
+          credentials: {
+            accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY')
+          },
+          endpoint: configService.get('AWS_COGNITO_AP_USER_POOL_URL')
+        }
+        return new CognitoIdentityProviderClient(config)
+      }
+    },
+    ApIdentityProviderClientService,
+    {
+      provide: AbstractIdentityProviderService,
+      useClass: IdentityProviderService
+    },
+    ClientCredentialGrantStrategyService
+  ],
+  controllers: [Oauth2Controller]
 })
-export class AuthModule {}
+export class AuthModule implements OnModuleInit {
+  constructor(
+    private readonly explorerService: StrategyExplorerService,
+    private readonly strategyRegistry: GrantStrategyRegistry,
+    private readonly identityProviderService: AbstractIdentityProviderService<IdentityProviderClientType>
+  ) {}
+  onModuleInit() {
+    const strategies = this.explorerService.explore<GrantStrategyInterface>(GRANT_STRATEGY_METADATA)
+    this.strategyRegistry.register(strategies)
+    const providers = this.explorerService.explore<IdentityProviderClientInterface>(IDENTITY_PROVIDER_METADATA)
+    this.identityProviderService.register(providers)
+  }
+}
