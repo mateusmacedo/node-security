abc4fb73db80da07875727f6ccfdd4278b85bc9d
Mateus Macedo Dos Anjos <mateus.capitani@qsaude.com.br>
2022-06-19 16:04:33 -0300

    test(auth): passport jwt auth strategy

---
+++ src/auth/services/strategies/jwt-strategy.spec.ts
@@ -0,0 +1,26 @@
+import { JwtStrategy } from '@app/auth/services/strategies'
+import { ConfigService } from '@nestjs/config'
+import { mock } from 'jest-mock-extended'
+
+describe('JwtStrategy', () => {
+  let jwtStrategy: JwtStrategy
+
+  beforeEach(async () => {
+    jwtStrategy = new JwtStrategy(
+      mock<ConfigService>({
+        get: jest.fn().mockReturnValue('dummy')
+      })
+    )
+  })
+
+  it('should be defined', () => {
+    expect(jwtStrategy).toBeDefined()
+  })
+  describe('validate', () => {
+    it('should return payload', async () => {
+      const payload = {}
+      const result = await jwtStrategy.validate(payload)
+      expect(result).toEqual(true)
+    })
+  })
+})
