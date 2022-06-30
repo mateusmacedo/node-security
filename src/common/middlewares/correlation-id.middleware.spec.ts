3913293251c539c8ffd46e036418be69eadfcece
Mateus Macedo Dos Anjos <mateus.capitani@qsaude.com.br>
2022-06-30 16:59:51 -0300

    refactor(common): simplify code complexibilty

--- src/common/middlewares/correlation-id.middleware.spec.ts
+++ src/common/middlewares/correlation-id.middleware.spec.ts
@@ -3,6 +3,8 @@ import { getMockReq, getMockRes } from '@jest-mock/express'
 import { Test } from '@nestjs/testing'
 import { NextFunction, Request, Response } from 'express'
 
+import { CORRELATION_ID_KEY } from '../constants/common.constant'
+
 describe('CorrelationIdMiddleware', () => {
   let req: Request
   let res: Response
@@ -27,18 +29,28 @@ describe('CorrelationIdMiddleware', () => {
     it('should generate a correlation id', () => {
       req = getMockReq()
       res = getMockRes().res
+      res.append = function (key, value) {
+        this.header[key] = value
+        return this
+      }
       next = getMockRes().next
       correlationIdMiddleware.use(req, res, next)
-      expect(req.headers['x-correlation-id']).toBeDefined()
+      expect(req.headers[CORRELATION_ID_KEY]).toBeDefined()
+      expect(res.header[CORRELATION_ID_KEY]).toBeDefined()
     })
     it('should preserve a request correlation id', () => {
       req = getMockReq()
-      res = getMockRes().res
+      res.append = function (key, value) {
+        this.header[key] = value
+        return this
+      }
       next = getMockRes().next
-      req.headers['Correlation-Id'] = 'dummy-correlation-id'
+      req.headers['x-correlation-id'] = 'dummy-correlation-id'
       correlationIdMiddleware.use(req, res, next)
       expect(req.headers['x-correlation-id']).toBeDefined()
       expect(req.headers['x-correlation-id']).toBe('dummy-correlation-id')
+      expect(res.header[CORRELATION_ID_KEY]).toBeDefined()
+      expect(res.header['x-correlation-id']).toBe('dummy-correlation-id')
     })
   })
 })
--- src/common/middlewares/correlation-id.middleware.ts
+++ src/common/middlewares/correlation-id.middleware.ts
@@ -1,3 +1,4 @@
+import { CORRELATION_ID_KEY } from '@app/common/constants'
 import { Injectable, NestMiddleware } from '@nestjs/common'
 import { NextFunction, Request, Response } from 'express'
 import { v4 as uuidv4 } from 'uuid'
@@ -5,22 +6,12 @@ import { v4 as uuidv4 } from 'uuid'
 @Injectable()
 export class CorrelationIdMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const correlationKeys = ['Correlation-Id', 'X-Correlation-Id', 'x-correlation-id']
-    const headers = req.headers
-    let correlationId: string
-    for (const header in headers) {
-      if (correlationKeys.includes(header)) {
-        correlationId = headers[header] as string
-        delete headers[header]
-        break
-      }
-    }
+    let correlationId = req.headers[CORRELATION_ID_KEY]
     if (!correlationId) {
       correlationId = uuidv4()
-      req.headers['x-correlation-id'] = correlationId
-    } else {
-      req.headers['x-correlation-id'] = correlationId
     }
+    req.headers[CORRELATION_ID_KEY] = correlationId
+    res.append(CORRELATION_ID_KEY, correlationId)
     next()
   }
 }
